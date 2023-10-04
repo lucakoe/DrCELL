d0c9221b6881708e8a4cc5417aab9a3d85d8861b
lucakoe <56564513+lucakoe@users.noreply.github.com>
2023-10-04 17:21:16 +0200

    working updated version for new file format

--- misc/umap_fullFrame_spatial_temporal_IL_custom_Luca.py
+++ misc/umap_fullFrame_spatial_temporal_IL_custom_Luca.py
@@ -13,9 +13,17 @@ import scipy.io as sio
 import matplotlib.pyplot as plt
 import seaborn as sns
 import os
+import pandas as pd
 
-allFiles = ['\\Naskampa\lts\\2P_PuffyPenguin\\2P_PuffyPenguin\\Umap_Analysis\\UMAPmatrixAllTasks_2532_Select.mat']
-outputPath = r'\\Naskampa\lts\2P_PuffyPenguin\2P_PuffyPenguin\Umap_Analysis'
+from bokeh.plotting import figure, show, output_notebook
+from bokeh.models import HoverTool, ColumnDataSource, CategoricalColorMapper
+from bokeh.palettes import Spectral10
+
+
+import util
+
+allFiles = [r'../data/Umap_2530_2532_Array.mat']
+outputPath = r'../data'
 #['C:\\Users\\despatin\\Downloads\\UMAPmatrixInnate2530.mat', 'C:\\Users\\despatin\\Downloads\\UMAPmatrixTaskWithoutDelay2530.mat', 'C:\\Users\\despatin\\Downloads\\UMAPmatrixTaskWithDelay2530.mat', 'C:\\Users\\despatin\\Downloads\\UMAPmatrixInnate22530.mat', 'C:\\Users\\despatin\\Downloads\\UMAPmatrixInnate2532.mat', 'C:\\Users\\despatin\\Downloads\\UMAPmatrixAudioTaskWithoutDelay2532.mat', 'C:\\Users\\despatin\\Downloads\\UMAPmatrixAudioTaskWithDelay2532.mat', 'C:\\Users\\despatin\\Downloads\\UMAPmatrixInnate22532.mat']
 # 
 # allFiles = ['Q:\\BpodImager\\umapClust\\20210309T2356_fullA.mat'] #first file
@@ -31,29 +39,44 @@ for cFile in allFiles:
     Y = np.array(g['UMAPmatrix'])
     # Y = np.array(g['Y'])
     #Y = np.transpose(Y)
-    
+    matrixLegend = np.array(g['matrixLegendArray'])
     # groups = np.array(g['redIdxAll'])
     # groups = np.reshape(groups,X.shape[0])
     # groups = groups.astype(int)
     
     #groups = np.array(g['groupIdx'])
-    
-    groups = np.array(g['matrixLegend'])
-    for iNeuron in range(0,len(groups)):
-        if groups[iNeuron,4] == 'Innate':
-            groups[iNeuron,4] = 1
-        elif groups[iNeuron,4] == 'Audio Task Without Delay':
-            groups[iNeuron,4] = 2
-        elif groups[iNeuron,4] == 'Audio Task with delay':
-            groups[iNeuron,4] = 3
-        elif groups[iNeuron,4] == 'Innate2':
-            groups[iNeuron,4] = 4
-    groups = np.reshape(groups[:,4],X.shape[0])
-    groups = groups.astype(int)    
+
+
+    # groups = np.array(g['matrixLegend'])
+    # for iNeuron in range(0,len(groups)):
+    #     if groups[iNeuron,4] == 'Innate':
+    #         groups[iNeuron,4] = 1
+    #     elif groups[iNeuron,4] == 'Audio Task Without Delay':
+    #         groups[iNeuron,4] = 2
+    #     elif groups[iNeuron,4] == 'Audio Task with delay':
+    #         groups[iNeuron,4] = 3
+    #     elif groups[iNeuron,4] == 'Innate2':
+    #         groups[iNeuron,4] = 4
+    # groups = np.reshape(groups[:,4],X.shape[0])
+    # groups = groups.astype(int)
     
     #g.close
-    
-    useIdx = np.invert(np.isnan(np.sum(Y, axis=0))) 
+
+    # Extract the 'UMAPmatrix' array from the loaded data
+    umap_matrix = g['UMAPmatrix']
+
+    # Create a Pandas DataFrame from 'UMAPmatrix'
+    umap_df = pd.DataFrame(umap_matrix)
+
+    # Extract the 'matrixLegendArray' array from the loaded data
+    matrix_legend_array = g['matrixLegendArray']
+
+    # Create a Pandas DataFrame from 'matrixLegendArray'
+    matrix_legend_df = pd.DataFrame(matrix_legend_array)
+    matrix_legend_df.columns = ["Animal", "Recording", "Neuron", "NbFrames", "Task", "RedNeurons", "ChoiceAUCs", "IsChoiceSelect", "StimAUCs", "IsStimSelect"]
+    matrix_legend_df['Task']=matrix_legend_df['Task'].astype(int)
+
+    useIdx = np.invert(np.isnan(np.sum(Y, axis=0)))
     Y = Y[:,useIdx]
     
     # run umap twice, once for spatial and once for temporal clusters
@@ -84,11 +107,11 @@ for cFile in allFiles:
     print('Umap vals: ' + str(tempUmapOut.shape))
     
     # show results
-    cIdx = np.random.permutation(groups.size)    
+    cIdx = np.random.permutation(matrix_legend_df["Task"].size)
     fig1, ax1 = plt.subplots()
     # cColor = sns.color_palette("Dark2", np.max([groups])+1);
-    cColor = sns.color_palette("Spectral", np.max([groups])+1);
-    ax1.scatter(tempUmapOut[cIdx, 0], tempUmapOut[cIdx, 1], c=[cColor[x] for x in groups[cIdx]], s=2)
+    cColor = sns.color_palette("Spectral", np.max([matrix_legend_df["Task"]])+1);
+    ax1.scatter(tempUmapOut[cIdx, 0], tempUmapOut[cIdx, 1], c=[cColor[x] for x in matrix_legend_df["Task"][cIdx]], s=2)
     #plt.axis('square')
     ax1.set_aspect('equal', adjustable = 'datalim')
     plt.show() 
@@ -99,6 +122,6 @@ for cFile in allFiles:
     #                  })
     
     # Save UMAP coordinates and labels to see in Matlab
-    umap_data = np.column_stack((tempUmapOut, groups))
+    umap_data = np.column_stack((tempUmapOut, matrix_legend_df["Task"]))
     file_name = os.path.basename(cFile)
     sio.savemat(r'\\Naskampa\lts\2P_PuffyPenguin\2P_PuffyPenguin\Umap_Analysis\output_' + file_name, {'umap_data': umap_data})
