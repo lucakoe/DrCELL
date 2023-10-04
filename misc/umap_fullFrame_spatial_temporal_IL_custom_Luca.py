bfa6e546257f28475d6f797ecd57bf1637f23cfd
lucakoe <56564513+lucakoe@users.noreply.github.com>
2023-10-04 12:56:44 +0200

    Initial version umap_fullFrame_spatial_temporal_IL from Playground/Irene 04.10.2023

---
+++ misc/umap_fullFrame_spatial_temporal_IL_custom_Luca.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 10 15:42:38 2020
+
+@author: Simon
+"""
+
+import numpy as np
+#import h5py
+import umap
+#import hdbscan
+import scipy.io as sio
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+
+allFiles = ['\\Naskampa\lts\\2P_PuffyPenguin\\2P_PuffyPenguin\\Umap_Analysis\\UMAPmatrixAllTasks_2532_Select.mat']
+outputPath = r'\\Naskampa\lts\2P_PuffyPenguin\2P_PuffyPenguin\Umap_Analysis'
+#['C:\\Users\\despatin\\Downloads\\UMAPmatrixInnate2530.mat', 'C:\\Users\\despatin\\Downloads\\UMAPmatrixTaskWithoutDelay2530.mat', 'C:\\Users\\despatin\\Downloads\\UMAPmatrixTaskWithDelay2530.mat', 'C:\\Users\\despatin\\Downloads\\UMAPmatrixInnate22530.mat', 'C:\\Users\\despatin\\Downloads\\UMAPmatrixInnate2532.mat', 'C:\\Users\\despatin\\Downloads\\UMAPmatrixAudioTaskWithoutDelay2532.mat', 'C:\\Users\\despatin\\Downloads\\UMAPmatrixAudioTaskWithDelay2532.mat', 'C:\\Users\\despatin\\Downloads\\UMAPmatrixInnate22532.mat']
+# 
+# allFiles = ['Q:\\BpodImager\\umapClust\\20210309T2356_fullA.mat'] #first file
+
+
+for cFile in allFiles:
+    # load data
+    print(cFile)
+    g = sio.loadmat(cFile)
+    #g = h5py.File(cFile,'r')
+    #X = np.array(g['X'])
+    X = np.array(g['UMAPmatrix'])
+    Y = np.array(g['UMAPmatrix'])
+    # Y = np.array(g['Y'])
+    #Y = np.transpose(Y)
+    
+    # groups = np.array(g['redIdxAll'])
+    # groups = np.reshape(groups,X.shape[0])
+    # groups = groups.astype(int)
+    
+    #groups = np.array(g['groupIdx'])
+    
+    groups = np.array(g['matrixLegend'])
+    for iNeuron in range(0,len(groups)):
+        if groups[iNeuron,4] == 'Innate':
+            groups[iNeuron,4] = 1
+        elif groups[iNeuron,4] == 'Audio Task Without Delay':
+            groups[iNeuron,4] = 2
+        elif groups[iNeuron,4] == 'Audio Task with delay':
+            groups[iNeuron,4] = 3
+        elif groups[iNeuron,4] == 'Innate2':
+            groups[iNeuron,4] = 4
+    groups = np.reshape(groups[:,4],X.shape[0])
+    groups = groups.astype(int)    
+    
+    #g.close
+    
+    useIdx = np.invert(np.isnan(np.sum(Y, axis=0))) 
+    Y = Y[:,useIdx]
+    
+    # run umap twice, once for spatial and once for temporal clusters
+    # umapOut = umap.UMAP(
+    #     n_neighbors=30,
+    #     min_dist=0.0,
+    #     n_components=2,
+    #     random_state=42,
+    #     ).fit_transform(X)
+    # print('Umap vals: ' + str(umapOut.shape))
+    
+    # show results
+    # cIdx = np.random.permutation(groups.size)    
+    # fig1, ax1 = plt.subplots()
+    # cColor = sns.color_palette("Dark2", np.max([groups])+1);
+    # ax1.scatter(umapOut[cIdx, 0], umapOut[cIdx, 1], c=[cColor[x] for x in groups[cIdx]], s=2)
+    # #plt.axis('square')
+    # ax1.set_aspect('equal', adjustable = 'datalim')
+    # plt.show() 
+    
+    # temporal clusters
+    tempUmapOut = umap.UMAP(
+        n_neighbors=30,
+        min_dist=0.0,
+        n_components=2,
+        random_state=42,
+        ).fit_transform(Y)
+    print('Umap vals: ' + str(tempUmapOut.shape))
+    
+    # show results
+    cIdx = np.random.permutation(groups.size)    
+    fig1, ax1 = plt.subplots()
+    # cColor = sns.color_palette("Dark2", np.max([groups])+1);
+    cColor = sns.color_palette("Spectral", np.max([groups])+1);
+    ax1.scatter(tempUmapOut[cIdx, 0], tempUmapOut[cIdx, 1], c=[cColor[x] for x in groups[cIdx]], s=2)
+    #plt.axis('square')
+    ax1.set_aspect('equal', adjustable = 'datalim')
+    plt.show() 
+    
+    # sio.savemat(cFile.replace('.mat','_umap_allAreas.mat'),
+    #                 {'umapOut':umapOut,
+    #                  'tempUmapOut':tempUmapOut,
+    #                  })
+    
+    # Save UMAP coordinates and labels to see in Matlab
+    umap_data = np.column_stack((tempUmapOut, groups))
+    file_name = os.path.basename(cFile)
+    sio.savemat(r'\\Naskampa\lts\2P_PuffyPenguin\2P_PuffyPenguin\Umap_Analysis\output_' + file_name, {'umap_data': umap_data})
